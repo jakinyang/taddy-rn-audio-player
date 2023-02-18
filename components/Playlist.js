9d08249ba04b8d188cb7830add8300a7b2bb2778
jakinyang <109392405+jakinyang@users.noreply.github.com>
2023-02-18 10:56:21 -0800

    --add playlist and dummy track component

---
+++ components/Playlist.js
@@ -0,0 +1,34 @@
+import { View, Text, FlatList } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import TrackPlayer from 'react-native-track-player';
+import { Track } from './Track';
+
+export function Playlist() {
+  const [tracks, setTracks] = useState([]);
+  
+  useEffect(() => {
+    async function getTracks() {
+      const queue = await TrackPlayer.getQueue();
+      setTracks(queue)
+    };
+    getTracks();
+  }, []);
+  if (!tracks) {
+    return (
+      <View>
+        <Text>Loading...</Text>
+      </View>
+    )
+  }
+
+  return (
+    <View>
+      <FlatList 
+        data={tracks}
+        renderItem={({item}) => (
+          <Track title={item.title} />
+        )}
+      />
+    </View>
+  )
+}
---
+++ components/Track.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export function Track({ title }) {
+  return (
+    <View>
+      <Text>{title}</Text>
+    </View>
+  )
+}
